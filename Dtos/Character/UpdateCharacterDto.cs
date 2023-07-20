90b8cedac0dec4a6c69f960cbe460470f1dd3676
DanielaSGomez <danielagomez992@hotmail.com>
2023-07-20 12:04:22 -0300

    new controllers

--- Dtos/Character/UpdateCharacterDto.cs
+++ Dtos/Character/UpdateCharacterDto.cs
@@ -6,8 +6,9 @@ using dotnet_rpg.Models;
 
 namespace dotnet_rpg.Dtos.Character
 {
-    public class AddCharacterRequestDto
+    public class UpdateCharacterDto
     {
+        public int Id { get; set; }
         public string Name { get; set; } = "Inira";
         public int HitPoints { get; set; } = 100;
         public int Strenght { get; set; } = 10;
--- Services/CharacterService/ICharacterService.cs
+++ Services/CharacterService/ICharacterService.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using dotnet_rpg.Models;
+using dotnet_rpg.Dtos.Character;
 
 namespace dotnet_rpg.Services.CharacterService
 {
     public interface ICharacterService
     {
         //responde con una task de srvice response del tipo list characters
-        Task <ServiceResponse<List<Character>>> GetAllCharacters();
-        Task <ServiceResponse<Character>> GetCharacterById(int id);
-        Task <ServiceResponse<List<Character>>> AddCharacter(Character newCharacter);
+        Task <ServiceResponse<List<GetCharacterResponseDto>>> GetAllCharacters();
+        Task <ServiceResponse<GetCharacterResponseDto>> GetCharacterById(int id);
+        Task <ServiceResponse<List<GetCharacterResponseDto>>> AddCharacter(AddCharacterRequestDto newCharacter);
+        Task <ServiceResponse<GetCharacterResponseDto>> UpdateCharacter(UpdateCharacterDto updateCharacter);
+
         
     }
 }
